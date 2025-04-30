2f70c93455e60f91ccb4bf27cd9239bf1e16ff0c
Loïc E. <lolocomotive181@gmail.com>
2025-04-30 23:23:37 +0200

    Fix Button not showing sometimes (#1)

--- lib/src/pages/crop_page.dart
+++ lib/src/pages/crop_page.dart
@@ -38,30 +38,35 @@ class _CropPageState extends State<CropPage> {
         //TODO make loading less abrupt
         children: [
           Expanded(
-            child: ExtendedImage.file(
-              File(widget.imagePath),
-              fit: BoxFit.contain,
-              beforePaintImage: (canvas, rect, image, paint) {
-                CheckerPainter.checkerPainter(canvas, rect, context);
-                return false;
-              },
-              mode: ExtendedImageMode.editor,
-              extendedImageEditorKey: widget.editorKey,
-              cacheRawData: true,
-
-              initEditorConfigHandler: (state) {
-                return EditorConfig(
-                  lineHeight: 3,
-                  lineColor: Theme.of(context).colorScheme.primary.withAlpha(100),
-                  animationDuration: const Duration(milliseconds: 100),
-                  maxScale: 8.0,
-                  cropRectPadding: const EdgeInsets.all(40.0),
-                  hitTestSize: 80.0,
-                  cropAspectRatio: null,
-                  cornerColor: Theme.of(context).colorScheme.primary,
-                  cornerSize: const Size(30, 5),
-                );
-              },
+            child: Container(
+              // The clip and empty BoxDecoration is intentional, sometimes the done button doesn't appear otherwise
+              // See: https://github.com/lolocomotive/stickers/issues/1
+              clipBehavior: Clip.antiAlias,
+              decoration: const BoxDecoration(),
+              child: ExtendedImage.file(
+                File(widget.imagePath),
+                fit: BoxFit.contain,
+                beforePaintImage: (canvas, rect, image, paint) {
+                  CheckerPainter.checkerPainter(canvas, rect, context);
+                  return false;
+                },
+                mode: ExtendedImageMode.editor,
+                extendedImageEditorKey: widget.editorKey,
+                cacheRawData: true,
+                initEditorConfigHandler: (state) {
+                  return EditorConfig(
+                    lineHeight: 3,
+                    lineColor: Theme.of(context).colorScheme.primary.withAlpha(100),
+                    animationDuration: const Duration(milliseconds: 100),
+                    maxScale: 8.0,
+                    cropRectPadding: const EdgeInsets.all(40.0),
+                    hitTestSize: 80.0,
+                    cropAspectRatio: null,
+                    cornerColor: Theme.of(context).colorScheme.primary,
+                    cornerSize: const Size(30, 5),
+                  );
+                },
+              ),
             ),
           ),
           Padding(
@@ -69,8 +74,7 @@ class _CropPageState extends State<CropPage> {
             child: ElevatedButton(
               onPressed: () async {
                 final state = widget.editorKey.currentState!;
-                final cropped = await cropSticker(
-                    state.getCropRect()!, state.rawImageData, widget.pack, widget.index);
+                final cropped = await cropSticker(state.getCropRect()!, state.rawImageData, widget.pack, widget.index);
                 final output = await saveTemp(cropped);
                 if (!context.mounted) return;
                 Navigator.of(context).pushNamed(
@@ -93,6 +97,7 @@ class _CropPageState extends State<CropPage> {
 
 class EditArguments {
   StickerPack pack;
+
   // Index 30 is tray icon
   int index;
   String imagePath;
