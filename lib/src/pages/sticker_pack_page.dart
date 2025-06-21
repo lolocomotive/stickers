a31cd0fb62d9b7367f2c5aeb97935b71125a4076
Loïc E. <lolocomotive181@gmail.com>
2025-06-21 12:01:40 +0200

    Add missing localizations

--- lib/src/pages/sticker_pack_page.dart
+++ lib/src/pages/sticker_pack_page.dart
@@ -46,7 +46,9 @@ class StickerPackPageState extends State<StickerPackPage> {
               IconButton(
                 tooltip: AppLocalizations.of(context)!.delete,
                 onPressed: () {
-                  showDialog<bool>(context: context, builder: (context) => DeleteConfirmDialog(widget.pack.title)).then(
+                  showDialog<bool>(
+                      context: context,
+                      builder: (context) => DeleteConfirmDialog(widget.pack.title)).then(
                     (value) async {
                       if (value == true) {
                         packs.remove(widget.pack);
@@ -90,7 +92,8 @@ class StickerPackPageState extends State<StickerPackPage> {
                               ? null
                               : () async {
                                   final ImagePicker picker = ImagePicker();
-                                  final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+                                  final XFile? image =
+                                      await picker.pickImage(source: ImageSource.gallery);
                                   if (image == null) return; //TODO add Snackbar warning
                                   if (!context.mounted) return;
 
@@ -115,13 +118,15 @@ class StickerPackPageState extends State<StickerPackPage> {
                     return Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(24),
-                        color: Color.lerp(
-                            Theme.of(context).colorScheme.primary, Theme.of(context).colorScheme.surface, .7),
+                        color: Color.lerp(Theme.of(context).colorScheme.primary,
+                            Theme.of(context).colorScheme.surface, .7),
                         boxShadow: [
                           BoxShadow(
                             offset: Offset(1, 1),
                             blurRadius: 3,
-                            color: Theme.of(context).brightness == Brightness.light ? Colors.black26 : Colors.black12,
+                            color: Theme.of(context).brightness == Brightness.light
+                                ? Colors.black26
+                                : Colors.black12,
                           )
                         ],
                       ),
@@ -166,7 +171,7 @@ class StickerPackPageState extends State<StickerPackPage> {
                   Opacity(
                     opacity: .7,
                     child: Text(
-                      "Cannot add stickers to animated packs (yet)",
+                      AppLocalizations.of(context)!.cannotAddToAnimated,
                       textAlign: TextAlign.center,
                     ),
                   ),
